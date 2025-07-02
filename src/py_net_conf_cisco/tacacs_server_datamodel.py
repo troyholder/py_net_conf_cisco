196f8d0a9060473b30f047f8e3b2ecebf36cf146
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-07-02 17:08:04 -0400

    Initial TacacsServerGroupConfig with initial creation testing.

--- src/py_net_conf_cisco/tacacs_server_datamodel.py
+++ src/py_net_conf_cisco/tacacs_server_datamodel.py
@@ -4,8 +4,10 @@ Data models for Cisco tacacs server configuration options.
 This module provides dataclasses and enums for structuring tacacs server configuration options.
 """
 
+import collections
 from dataclasses import dataclass
 from ipaddress import IPv4Address, IPv6Address
+from typing import Optional
 
 
 @dataclass
@@ -28,3 +30,30 @@ class TacacsServerConfig:
             )
         if not isinstance(self.encrpyted_string, str):
             raise TypeError("encrpyted_string must be a string")
+
+
+@dataclass
+class TacacsServerGroupConfig:
+    group_name: str
+    servers: list[TacacsServerConfig]
+    vrf: Optional[str] = None
+    interface: Optional[str] = None
+
+    def __post_init__(self):
+        if len(self.servers) == 0:
+            raise TypeError("servers must be a non-empty list")
+        for index, server in enumerate(self.servers):
+            if type(server) is not TacacsServerConfig:
+                raise TypeError(
+                    f"server at {str(index)} of servers is not a TacacsServerGroupConfig"
+                )
+        duplicates = [
+            server_ip
+            for server_ip, count in collections.Counter(
+                str(server.ip_address) for server in self.servers
+            ).items()
+            if count > 1
+        ]
+        raise TypeError(
+            f"duplicate servers in servers list: {', '.join(duplicates)}"
+        )
--- tests/test_tacacs_server_datamodel.py
+++ tests/test_tacacs_server_datamodel.py
@@ -7,7 +7,10 @@ from ipaddress import IPv4Address
 
 import pytest
 
-from py_net_conf_cisco.tacacs_server_datamodel import TacacsServerConfig
+from py_net_conf_cisco.tacacs_server_datamodel import (
+    TacacsServerConfig,
+    TacacsServerGroupConfig,
+)
 
 server1 = IPv4Address("1.1.1.1")
 server2 = IPv4Address("2.2.2.2")
@@ -60,7 +63,7 @@ class TestTacacsServerConfig:
 
     @pytest.mark.parametrize("kwargs, warning", failing_cases)
     def test_failing_creation(self, kwargs, warning):
-        """Test a failing creation"""
+        """Test failing creations"""
         with pytest.raises(TypeError, match=warning):
             interface = TacacsServerConfig(**kwargs)  # pyright: ignore
             return interface
@@ -71,3 +74,147 @@ class TestTacacsServerConfig:
         )
         assert tacas_server.ip_address == server1
         assert tacas_server.encrpyted_string == encrpyted_string
+
+
+class TestTacacsServerGroupConfig:
+    failing_cases = [
+        (
+            {},
+            re.escape(
+                "TacacsServerGroupConfig.__init__() missing 2 required positional arguments: 'group_name' and 'servers'"
+            ),
+        ),
+        (
+            {
+                "group_name": "tacas_group",
+            },
+            re.escape(
+                "TacacsServerGroupConfig.__init__() missing 1 required positional argument: 'servers'"
+            ),
+        ),
+        (
+            {
+                "servers": [],
+            },
+            re.escape(
+                "TacacsServerGroupConfig.__init__() missing 1 required positional argument: 'group_name'"
+            ),
+        ),
+        (
+            {
+                "group_name": "tacas_group",
+                "servers": [],
+            },
+            re.escape("servers must be a non-empty list"),
+        ),
+        (
+            {
+                "group_name": "tacas_group",
+                "servers": ["1.1.1.1"],
+            },
+            re.escape(
+                "server at 0 of servers is not a TacacsServerGroupConfig"
+            ),
+        ),
+        (
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    "1.1.1.1",
+                ],
+            },
+            re.escape(
+                "server at 1 of servers is not a TacacsServerGroupConfig"
+            ),
+        ),
+        (
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server1, encrpyted_string),
+                ],
+            },
+            re.escape("duplicate servers in servers list: 1.1.1.1"),
+        ),
+        (
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server2, encrpyted_string),
+                    TacacsServerConfig(server2, encrpyted_string),
+                ],
+            },
+            re.escape("duplicate servers in servers list: 1.1.1.1, 2.2.2.2"),
+        ),
+    ]
+
+    @pytest.mark.parametrize("kwargs, warning", failing_cases)
+    def test_failing_creations(self, kwargs, warning):
+        """Test failing creations"""
+        with pytest.raises(TypeError, match=warning):
+            interface = TacacsServerGroupConfig(**kwargs)  # pyright: ignore
+            return interface
+
+    working_cases = [
+        (  # Minimum requirements
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                ],
+            },
+            [],
+        ),
+        (  # Multiple servers
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server2, encrpyted_string),
+                ],
+            },
+            [],
+        ),
+        (  # Multiple servers with vrf
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server2, encrpyted_string),
+                ],
+                "vrf": "mgmt",
+            },
+            [],
+        ),
+        (  # Multiple servers with interface
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server2, encrpyted_string),
+                ],
+                "interface": "GigabitEthernet1",
+            },
+            [],
+        ),
+        (  # Multiple servers with vrf and interface
+            {
+                "group_name": "tacas_group",
+                "servers": [
+                    TacacsServerConfig(server1, encrpyted_string),
+                    TacacsServerConfig(server2, encrpyted_string),
+                ],
+                "vrf": "mgmt",
+                "interface": "GigabitEthernet1",
+            },
+            [],
+        ),
+    ]
+
+    @pytest.mark.parametrize("kwargs, expected_lines", working_cases)
+    def test_working_creations(self, kwargs, expected_lines):
+        pass
