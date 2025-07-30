5786bbbbb2a38aa9751144ba7b266af14dc98cd4
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-07-30 16:17:21 -0400

    Added TacacsServerPrivateConfig to represent "server-private ..." configuration lines in a "aaa group server tacacs+ tacas_group" Added a list of TacacsServerPrivateConfig objects to the TacacsServerGroupConfig variable server_private_list. This completes enough for what I need at this time - I think.

--- src/py_net_conf_cisco/tacacs_server_datamodel.py
+++ src/py_net_conf_cisco/tacacs_server_datamodel.py
@@ -78,11 +78,13 @@ As of 17.9.5 the option tree looks like:
     └── timeout <seconds>
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from ipaddress import IPv4Address, IPv6Address
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from .interface_datamodel import Interface
+
+_KEY_MODES: List[Union[int, None]] = [0, 6, 7, None]
 
 
 @dataclass
@@ -108,16 +110,91 @@ class TacacsServerConfig:
 
 
 @dataclass
+class TacacsServerPrivateConfig:
+    """
+    Represents a tacacs group's server-private configuration.
+
+    Attributes:
+            ip_address: IP address (ipaddress.IPv4Address|ipaddress.IPv6Address).
+            encrpyted_string: Encryption key.
+    """
+
+    ip_address: Optional[IPv4Address | IPv6Address] = None
+    fqdn: Optional[str] = None
+    key_mode: Optional[int] = None
+    key: Optional[str] = None
+
+    def __post_init__(self):
+        if self.ip_address is None and self.fqdn is None:
+            raise TypeError("ip_address or fqdn must be given")
+        if self.ip_address is not None and not isinstance(
+            self.ip_address, (IPv4Address, IPv6Address)
+        ):
+            raise TypeError(
+                "ip_address must be an IPv4Address or IPv6Address instance"
+            )
+        if self.fqdn is not None and not isinstance(self.fqdn, str):
+            raise TypeError("fqdn must be a string")
+        if self.ip_address is not None and self.fqdn is not None:
+            raise TypeError("must not set ip_address and fqdn")
+        if self.key_mode not in _KEY_MODES:
+            raise TypeError("key_mode must be 0, 6, 7, or None")
+        if self.key is not None and not isinstance(self.key, str):
+            raise TypeError(
+                "key must be a string that does not start with a space"
+            )
+        if self.key is not None and self.key[0] == " ":
+            raise TypeError(
+                "key must not start with spaces to avoid possible issues"
+            )
+
+    def to_config_lines(self):
+        line = " server-private "
+        if self.ip_address:
+            line += str(self.ip_address)
+        else:
+            line += "fqdn " + str(self.fqdn)
+        if isinstance(self.key_mode, int):
+            line += " " + str(self.key_mode)
+        if isinstance(self.key, str):
+            line += " " + self.key
+
+        return [line]
+
+
+@dataclass
 class TacacsServerGroupConfig:
     name: str
     vrf: Optional[str] = None
     source_interface: Optional[Interface] = None
+    server_private_list: List[Union[TacacsServerPrivateConfig, None]] = field(
+        default_factory=list
+    )
 
     def __post_init__(self):
-        pass
+        if self.name is not None and not isinstance(self.name, str):
+            raise TypeError("name must be a string")
+        if self.vrf is not None and not isinstance(self.vrf, str):
+            raise TypeError("vrf must be a string")
+        if self.source_interface is not None and not isinstance(
+            self.source_interface, Interface
+        ):
+            raise TypeError("source_interface must be an Interface")
+        if not isinstance(self.server_private_list, list) or not all(
+            isinstance(server, TacacsServerPrivateConfig)
+            for server in self.server_private_list
+        ):
+            raise TypeError(
+                "server_private_list must either be an empty list or list "
+                + "of TacacsServerPrivateConfig objects"
+            )
 
     def to_config_lines(self) -> List[str]:
         lines = [f"aaa group server tacacs+ {self.name}"]
+        if self.server_private_list:
+            for server in self.server_private_list:
+                if server is not None:
+                    lines += server.to_config_lines()
         if self.vrf:
             lines.append(f" ip vrf forwarding {self.vrf}")
         if self.source_interface:
