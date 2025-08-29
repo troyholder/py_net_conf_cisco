10bec021d60e81a185201b13d2c1c443372d1087
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-08-29 16:53:44 -0400

    Added a file with variables that can be used for unit test cases.

---
+++ tests/unit/vars.py
@@ -0,0 +1,8 @@
+from ipaddress import IPv4Address, IPv6Address
+
+server_1_ipv4_address = IPv4Address("192.168.1.1")
+server_1_ipv6_address = IPv6Address("2001:db8::1")
+server_2_ipv4_address = IPv4Address("192.168.2.1")
+server_2_ipv6_address = IPv6Address("2001:db8::2")
+encrpyted_string_1 = "encrpyted_string_1"
+encrpyted_string_2 = "encrpyted_string_2"
