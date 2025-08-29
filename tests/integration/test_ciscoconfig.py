e255d9f9932924346bc568f1e0538b67f8a1f06b
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-08-29 15:43:43 -0400

    Moved logging config tests and imports to a different file.

--- tests/integration/test_ciscoconfig.py
+++ tests/integration/test_ciscoconfig.py
@@ -5,7 +5,6 @@ import sample1
 
 from py_net_conf_cisco import CiscoConfig, InterfaceConfig
 from py_net_conf_cisco.interfaceconfig import Interface, InterfaceType
-from py_net_conf_cisco.loggingconfig import LoggingConfig
 from py_net_conf_cisco.radiusserverconfig import RadiusServerConfig
 
 
@@ -655,90 +654,3 @@ class TestCiscoConfig:
     ):
         config_from_file.radius_servers = radius_servers
         assert config_from_file.radius_servers == radius_servers
-
-    get_logging_server_configs = [
-        ("empty_config", []),
-        (
-            "config_from_file",
-            [
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.1.50"),
-                ),
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.1.51"),
-                    vrf="Blue",
-                ),
-            ],
-        ),
-    ]
-
-    @pytest.mark.parametrize(
-        "config, logging_configs", get_logging_server_configs
-    )
-    def test_logging_servers_property(
-        self,
-        config,
-        logging_configs: list[LoggingConfig],
-        request,
-    ):
-        assert (
-            request.getfixturevalue(config).logging_servers == logging_configs
-        )
-
-    set_logging_server_configs = [
-        (
-            "empty_config",
-            [
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.1.50"),
-                ),
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.1.51"),
-                    vrf="Blue",
-                ),
-            ],
-        ),
-        (
-            "config_from_file",
-            [],
-        ),
-        (
-            "config_from_file",
-            [
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.2.51"),
-                    vrf="Blue",
-                ),
-            ],
-        ),
-        (
-            "config_from_file",
-            [
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.2.51"),
-                    vrf="Blue",
-                ),
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.2.52"),
-                    vrf="Blue",
-                ),
-                LoggingConfig(
-                    syslog_ip_address=IPv4Address("192.168.2.53"),
-                    vrf="Blue",
-                ),
-            ],
-        ),
-    ]
-
-    @pytest.mark.parametrize(
-        "config, logging_configs", set_logging_server_configs
-    )
-    def test_logging_servers_setter(
-        self,
-        config,
-        logging_configs: list[LoggingConfig],
-        request,
-    ):
-        config = request.getfixturevalue(config)
-        config.logging_servers = logging_configs
-        assert config.logging_servers == logging_configs
