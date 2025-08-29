b6c7d7c5d87ebf01acf392f635b757648277b5e8
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-08-29 15:49:11 -0400

    Moved test interface config and secondary IP tests to a new file.

---
+++ tests/integration/test_ciscoconfig_interface.py
@@ -0,0 +1,449 @@
+from ipaddress import IPv4Interface
+
+import pytest
+
+from py_net_conf_cisco import CiscoConfig, InterfaceConfig
+from py_net_conf_cisco.interfaceconfig import Interface, InterfaceType
+
+
+class TestCiscoConfig:
+    """
+    Test class for the CiscoConfig module interface config methods
+    """
+
+    @pytest.mark.parametrize(
+        "interface,expected",
+        [
+            (
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="10",
+                    )
+                ),
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="10",
+                    ),
+                    description="Server VLAN",
+                    ip_address=IPv4Interface("10.0.10.1/24"),
+                    dhcp_assigned=False,
+                    shutdown=False,
+                ),
+            ),
+            (
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.GIGABITETHERNET,
+                        interface_number="0/3",
+                    )
+                ),
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.GIGABITETHERNET,
+                        interface_number="0/3",
+                    ),
+                    description="DHCP Test Interface",
+                    ip_address=None,
+                    dhcp_assigned=True,
+                    shutdown=False,
+                ),
+            ),
+            (
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="20",
+                    ),
+                ),
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="20",
+                    ),
+                    description="VRF VLAN",
+                    ip_address=IPv4Interface("10.10.10.1/24"),
+                    dhcp_assigned=False,
+                    shutdown=False,
+                    vrf="Blue",
+                ),
+            ),
+            (
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="30",
+                    ),
+                ),
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="30",
+                    ),
+                    description="VRF VLAN with secondary IPs",
+                    ip_address=IPv4Interface("10.20.10.1/24"),
+                    dhcp_assigned=False,
+                    shutdown=False,
+                    vrf="Blue",
+                    secondary_ip_addresses=[
+                        IPv4Interface("10.20.20.1/24"),
+                    ],
+                ),
+            ),
+        ],
+    )
+    def test_getting_interface(self, config_from_file, interface, expected):
+        assert config_from_file.get_interface(interface) == expected
+
+    def test_interface_that_does_not_exist(self, empty_config):
+        interface = InterfaceConfig(
+            Interface(
+                interface_type=InterfaceType.LOOPBACK,
+                interface_number="1234",
+            ),
+        )
+        assert empty_config.get_interface(interface) is None
+
+    @pytest.mark.parametrize(
+        "interface",
+        [
+            # No change to the secondary IPs
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="30",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                ],
+            ),
+            # Change interface to using DHCP
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="30",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                dhcp_assigned=True,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                ],
+            ),
+            # Change secondaries
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="30",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.10.10.1/24"),
+                ],
+            ),
+            # Removal of secondaries,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="30",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+            ),
+            # More secondaries,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="30",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                    IPv4Interface("10.20.21.1/24"),
+                ],
+            ),
+            # Less secondaries,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="40",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.30.10.1/24"),
+                ],
+            ),
+            # Change and more secondaries,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="30",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("2.2.2.2/24"),
+                    IPv4Interface("3.3.3.3/24"),
+                ],
+            ),
+            # Change and less secondaries,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="40",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                ],
+            ),
+            # Add secondary IP when there was none
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.VLAN,
+                    interface_number="1",
+                ),
+                ip_address=IPv4Interface("192.168.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=False,
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                ],
+            ),
+            # Start with blank interface,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.GIGABITETHERNET,
+                    interface_number="0/4",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                ],
+            ),
+            # Start with no interface,
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.GIGABITETHERNET,
+                    interface_number="0/5",
+                ),
+                description="Red VRF VLAN with secondary IPs",
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=True,
+                vrf="Red,",
+                secondary_ip_addresses=[
+                    IPv4Interface("10.20.20.1/24"),
+                ],
+            ),
+            # Test ip change from DHCP to static
+            InterfaceConfig(
+                Interface(
+                    interface_type=InterfaceType.GIGABITETHERNET,
+                    interface_number="0/3",
+                ),
+                ip_address=IPv4Interface("1.1.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=False,
+            ),
+            # Test enabling and interface
+            InterfaceConfig(
+                Interface(
+                    InterfaceType.VLAN,
+                    interface_number="1",
+                ),
+                ip_address=IPv4Interface("192.168.1.1/24"),
+                dhcp_assigned=False,
+                shutdown=False,
+            ),
+        ],
+    )
+    def test_setting_interface(self, config_from_file, interface):
+        assert config_from_file.set_interface(interface) is True
+        assert config_from_file.get_interface(interface) == interface
+
+    def test__unexpected_config_line(self, empty_config):
+        with pytest.raises(ValueError):
+            empty_config._unexpected_config_line(
+                empty_config._parsed_config.config_objs[0]
+            )
+            return empty_config
+
+    def test__unexpected_config_line_with_config_obj(self, empty_config):
+        with pytest.raises(ValueError):
+            empty_config._unexpected_config_line(
+                empty_config._parsed_config.config_objs[0]
+            )
+            return empty_config
+
+    def test_multiple_interface_lines(self, empty_config):
+        line = empty_config._parsed_config.config_objs[0]
+        line.insert_after("interface GigabitEthernet0/6")
+        line.insert_after("interface GigabitEthernet0/6")
+        interface = InterfaceConfig(
+            Interface(
+                interface_type=InterfaceType.GIGABITETHERNET,
+                interface_number="0/6",
+            ),
+        )
+        with pytest.raises(ValueError, match="Found multiple interfaces"):
+            empty_config._unexpected_config_line(
+                empty_config.get_interface(interface)
+            )
+            pass
+        with pytest.raises(ValueError, match="Found multiple interfaces"):
+            empty_config._unexpected_config_line(
+                empty_config.set_interface(interface)
+            )
+            return empty_config
+
+    @pytest.mark.parametrize(
+        "interface,lines,match",
+        [
+            (  # Test for an unknown interface configuration line
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN, interface_number="1"
+                    ),
+                ),
+                [
+                    " bogus_line",
+                ],
+                "Unexpected config line:  bogus_line",
+            ),
+            (  # Test ip config with 5 words that are not a known option)
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="1",
+                    ),
+                ),
+                [
+                    " ip address 192.168.1.1 255.255.255.0 bogus",
+                ],
+                "Unexpected config line:  ip address 192.168.1.1 255.255.255.0 bogus",
+            ),
+            (  # Test ip config with known option)
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="1",
+                    ),
+                ),
+                [
+                    " ip address bogus",
+                ],
+                "Unexpected config line:  ip address bogus",
+            ),
+        ],
+    )
+    def test_get_interface_config_exceptions(
+        self, config_from_file, interface, lines, match
+    ):
+        interface_line = config_from_file._find_interface_lines(interface)[0]
+        for line in lines:
+            interface_line.insert_after(line)
+        with pytest.raises(ValueError, match=match):
+            config_from_file.get_interface(interface)
+
+    @pytest.mark.parametrize(
+        "interface,lines,match",
+        [
+            (  # Test for an unknown interface configuration line
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="1",
+                    ),
+                ),
+                [
+                    " bogus_line",
+                ],
+                "Unexpected config line:  bogus_line",
+            ),
+            (  # Test ip config with 5 words that are not a known option
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="1",
+                    ),
+                ),
+                [
+                    " ip address 192.168.1.1 255.255.255.0 bogus",
+                ],
+                "Unexpected config line:  ip address 192.168.1.1 255.255.255.0 bogus",
+            ),
+            (  # Test ip config with known option
+                InterfaceConfig(
+                    Interface(
+                        interface_type=InterfaceType.VLAN,
+                        interface_number="1",
+                    ),
+                ),
+                [
+                    " ip address bogus",
+                ],
+                "Unexpected config line:  ip address bogus",
+            ),
+        ],
+    )
+    def test_set_interface_config_exceptions(
+        self, config_from_file, interface, lines, match
+    ):
+        interface_line = config_from_file._find_interface_lines(interface)[0]
+        for line in lines:
+            interface_line.insert_after(line)
+        with pytest.raises(ValueError, match=match):
+            config_from_file.set_interface(interface)
+
+    def test_add_secondary_with_no_primary(self, config_from_file: CiscoConfig):
+        interface = InterfaceConfig(
+            Interface(
+                interface_type=InterfaceType.VLAN,
+                interface_number="1",
+            ),
+            secondary_ip_addresses=[
+                IPv4Interface("1.1.1.1/24"),
+            ],
+        )
+
+        exception_string = "Trying to add secondary IP with no primary IP"
+        interface_line = config_from_file._find_interface_lines(interface)[0]
+        ip_line = interface_line.re_search_children(r"ip address.*")[0]
+        ip_line.re_sub(r"ip address.*", "!")
+        config_from_file._parsed_config.commit()
+        with pytest.raises(ValueError, match=exception_string):
+            config_from_file.set_interface(interface)
